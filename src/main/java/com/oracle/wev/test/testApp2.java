c46a1b4d9d3dbe73ddf329d70ba94b63b8f7626a
FangJunBao <FangJunBao@qq.com>
2019-05-07 11:14:43 +0800

    岳不群提交了testApp2

---
+++ src/main/java/com/oracle/wev/test/testApp2.java
@@ -0,0 +1,10 @@
+package com.oracle.wev.test;
+
+public class testApp2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("Helloworld22");
+	}
+
+}
