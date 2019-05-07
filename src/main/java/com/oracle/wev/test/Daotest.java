d3e665e50a0adb44c4de6ab4283547b932bc2a67
FangJunBao <FangJunBao@qq.com>
2019-05-07 10:34:43 +0800

    第一次提交ssm

---
+++ src/main/java/com/oracle/wev/test/Daotest.java
@@ -0,0 +1,37 @@
+package com.oracle.wev.test;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.github.pagehelper.PageHelper;
+import com.oracle.web.mapper.MonsterMapper;
+import com.oralce.web.bean.subMonster;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
+public class Daotest {
+	@Autowired
+	private MonsterMapper monsterMapper;
+	
+	@Test
+	public void test(){
+		
+		PageHelper.startPage(1,5);
+		
+		List<subMonster> list=this.monsterMapper.sesectAllBypage();
+		
+		for (subMonster subMonster : list) {
+			
+			System.out.println(subMonster);
+			
+		}
+		
+		
+	}
+
+}
